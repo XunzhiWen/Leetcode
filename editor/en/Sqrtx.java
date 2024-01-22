810c68144436267cb9ce5db8c76fd89995e3c9ae
Xunzhi Wen <xwen75@gatech.edu>
2024-01-22 10:40:22 -0500

    BinarySearch finshed

--- editor/en/Sqrtx.java
+++ editor/en/Sqrtx.java
@@ -43,7 +43,24 @@
       //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int mySqrt(int x) {
-        
+        if  (x<2){
+            return x;
+        }
+        int left =2, right = x/2;
+        while (left<=right){
+            int mid = left+(right-left)/2;
+            long num = (long)mid*mid;
+            if (num==x){
+                return mid;
+            }
+            else if (num<x){
+                left=mid+1;
+            }
+            else {
+                right=mid-1;
+            }
+        }
+        return right;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
