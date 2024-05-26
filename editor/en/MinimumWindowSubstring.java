d0874a24dc35ade6464891db65a66004f50acaf3
Xunzhi Wen <xwen75@gatech.edu>
2024-05-26 14:54:07 -0400

    又写了一题

--- editor/en/MinimumWindowSubstring.java
+++ editor/en/MinimumWindowSubstring.java
@@ -1,4 +1,4 @@
-  //Given two strings s and t of lengths m and n respectively, return the minimum 
+//Given two strings s and t of lengths m and n respectively, return the minimum
 //window substring of s such that every character in t (including duplicates) is 
 //included in the window. If there is no such substring, return the empty string 
 //"". 
@@ -47,18 +47,56 @@
 //
 // Related Topics Hash Table String Sliding Window 👍 17386 👎 701
 
-  
-  package com.shuzijun.leetcode.editor.en;
-  public class MinimumWindowSubstring{
-      public static void main(String[] args) {
-           Solution solution = new MinimumWindowSubstring().new Solution();
-      }
-      //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public String minWindow(String s, String t) {
-        
+
+package com.shuzijun.leetcode.editor.en;
+
+import java.util.HashMap;
+
+public class MinimumWindowSubstring {
+    public static void main(String[] args) {
+        Solution solution = new MinimumWindowSubstring().new Solution();
     }
-}
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public String minWindow(String s, String t) {
+            HashMap<Character, Integer> window = new HashMap<>();
+            HashMap<Character, Integer> target = new HashMap<>();
+//            初始化，记录目标字符串中每个字符出现的次数
+            for (char c : t.toCharArray()) target.put(c, target.getOrDefault(c, 0) + 1);
+            int left = 0, right = 0, valid = 0;//出现次数已经匹配的字符串的个数
+            int minLen = Integer.MAX_VALUE, start = 0;//当前字符串长度初始化为无穷大，start是选中字符串的起点
+            while (right < s.length()) {
+                char c = s.charAt(right);
+                right++;
+                if (target.containsKey(c)) {//只有当遇到第一个包含在目标字符串中的字符时才开始记录滑窗
+                    window.put(c, window.getOrDefault(c, 0) + 1);
+                    if (target.get(c).equals(window.get(c))) {
+//                        说明该字符已经被完全匹配了
+                        valid++;
+                    }
+                }
+
+//                当窗口已经包含了所有目标字符时，开始缩小左端
+                while (valid == target.size()) {
+//                    更新当前最小滑窗的长度
+                    if (right - left < minLen) {
+                        minLen = right - left;
+                        start = left;
+                    }
+//                    查看如果缩小左端，滑窗是否依然有效
+                    char l = s.charAt(left);
+                    if (target.containsKey(l)) {
+                        window.put(l, window.get(l) - 1);
+                        if (window.get(l) < target.get(l)) valid--;
+                    }
+                    left++;
+                }
+            }
+
+            return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
+        }
+    }
 //leetcode submit region end(Prohibit modification and deletion)
 
-  }
+}
