a2acce23a34eaa0d615b9798da44829d852daff8
Xunzhi Wen <xwen75@gatech.edu>
2024-10-06 15:21:45 -0400

    Combination II finished

--- editor/en/CombinationSumIi.java
+++ editor/en/CombinationSumIi.java
@@ -1,4 +1,4 @@
-  //Given a collection of candidate numbers (candidates) and a target number (
+//Given a collection of candidate numbers (candidates) and a target number (
 //target), find all unique combinations in candidates where the candidate numbers sum 
 //to target. 
 //
@@ -42,18 +42,46 @@
 //
 // Related Topics Array Backtracking 👍 11107 👎 324
 
-  
-  package com.shuzijun.leetcode.editor.en;
-  public class CombinationSumIi{
-      public static void main(String[] args) {
-           Solution solution = new CombinationSumIi().new Solution();
-      }
-      //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        
+
+package com.shuzijun.leetcode.editor.en;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CombinationSumIi {
+    public static void main(String[] args) {
+        Solution solution = new CombinationSumIi().new Solution();
     }
-}
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+            List<List<Integer>> result = new ArrayList<>();
+            List<Integer> comb = new ArrayList<>();
+            Arrays.sort(candidates);
+            backtracking(result, comb, candidates, target, 0);
+            return result;
+        }
+
+        private void backtracking(List<List<Integer>> result, List<Integer> comb, int[] candidates, int remain, int curr) {
+            if (remain == 0) {
+                result.add(new ArrayList<>(comb));
+                return;
+            } else if (remain < 0) {
+                return;
+            }
+
+            for (int i = curr; i < candidates.length; i++) {
+//                The step below is the key to prevent duplicates
+                if (i > curr && candidates[i] == candidates[i - 1]) continue;
+                comb.add(candidates[i]);
+                this.backtracking(result, comb, candidates, remain - candidates[i], i + 1);
+                comb.remove(comb.size() - 1);
+
+            }
+        }
+    }
 //leetcode submit region end(Prohibit modification and deletion)
 
-  }
+}
