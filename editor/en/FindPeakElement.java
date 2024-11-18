2a76535de4e30417c7c92467c252d6e147b610cd
Xunzhi Wen <xwen75@gatech.edu>
2024-11-17 19:02:52 -0500

    528 finished

--- editor/en/FindPeakElement.java
+++ editor/en/FindPeakElement.java
@@ -50,8 +50,9 @@ public class FindPeakElement {
     class Solution {
         public int findPeakElement(int[] nums) {
             int l = 0, r = nums.length - 1;
+            int mid = 0;
             while (l < r) {
-                int mid = (l + r) / 2;
+                mid = (r + l) / 2;
                 if (nums[mid] > nums[mid + 1]) r = mid;
                 else l = mid + 1;
             }
