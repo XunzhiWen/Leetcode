7109e6f66b3a6fa3496385057b881043cbb1a2cf
Xunzhi Wen <xwen75@gatech.edu>
2024-11-23 23:03:56 -0500

    21 23 finished

--- editor/en/MergeTwoSortedLists.java
+++ editor/en/MergeTwoSortedLists.java
@@ -58,7 +58,7 @@ public class MergeTwoSortedLists {
      * }
      */
 
-    public class ListNode {
+/*    public class ListNode {
         int val;
         ListNode next;
 
@@ -73,31 +73,52 @@ public class MergeTwoSortedLists {
             this.val = val;
             this.next = next;
         }
-    }
+    }*/
 
     class Solution {
         public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-            ListNode head = new ListNode();
-            ListNode head0 = head;
-            while (list1 != null || list2 != null) {
-                if (list1 == null) {
-                    head.next = list2;
-                    break;
-                }
-                if (list2 == null) {
-                    head.next = list1;
-                    break;
-                }
-                if (list1.val <= list2.val) {
-                    head.next = list1;
+//            ListNode head = new ListNode();
+//            ListNode head0 = head;
+//            while (list1 != null || list2 != null) {
+//                if (list1 == null) {
+//                    head.next = list2;
+//                    break;
+//                }
+//                if (list2 == null) {
+//                    head.next = list1;
+//                    break;
+//                }
+//                if (list1.val <= list2.val) {
+//                    head.next = list1;
+//                    list1 = list1.next;
+//                } else {
+//                    head.next = list2;
+//                    list2 = list2.next;
+//                }
+//                head = head.next;
+//            }
+//            return head0.next;
+
+            ListNode newHead = new ListNode(-1);
+            ListNode ptr = newHead;
+            while (list1 != null && list2 != null) {
+                if (list1.val < list2.val) {
+                    ptr.next = list1;
                     list1 = list1.next;
                 } else {
-                    head.next = list2;
+                    ptr.next = list2;
                     list2 = list2.next;
                 }
-                head = head.next;
+                ptr = ptr.next;
             }
-            return head0.next;
+
+            if (list1 != null) {
+                ptr.next = list1;
+            }
+            if (list2 != null) {
+                ptr.next = list2;
+            }
+            return newHead.next;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
