06225e04c48fb4de94a6e5e4bca33dcdf5f7c4ca
Xunzhi Wen <xwen75@gatech.edu>
2025-01-28 11:15:08 -0500

    update

--- editor/en/[647]Palindromic Substrings.java
+++ editor/en/[647]Palindromic Substrings.java
@@ -1,4 +1,4 @@
-  //Given a string s, return the number of palindromic substrings in it. 
+//Given a string s, return the number of palindromic substrings in it.
 //
 // A string is a palindrome when it reads the same backward as forward. 
 //
@@ -31,18 +31,38 @@
 //
 // Related Topics Two Pointers String Dynamic Programming 👍 11001 👎 242
 
-  
-  package com.shuzijun.leetcode.editor.en;
-  public class PalindromicSubstrings{
-      public static void main(String[] args) {
-           Solution solution = new PalindromicSubstrings().new Solution();
-      }
-      //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public int countSubstrings(String s) {
-        
+
+package com.shuzijun.leetcode.editor.en;
+
+public class PalindromicSubstrings {
+    public static void main(String[] args) {
+        Solution solution = new PalindromicSubstrings().new Solution();
     }
-}
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int countSubstrings(String s) {
+/*//        布尔类型的dp[i][j]：表示区间范围[i,j] （注意是左闭右闭）的子串是否是回文子串，如果是dp[i][j]为true，否则为false。
+            char[] chars = s.toCharArray();
+            int len = chars.length;
+            boolean[][] dp = new boolean[len][len];
+            int result = 0;
+            for (int i = len - 1; i >= 0; i--) {
+                for (int j = i; j < len; j++) {
+                    if (chars[i] == chars[j]) {
+                        if (j - i <= 1) { // 情况一 和 情况二
+                            result++;
+                            dp[i][j] = true;
+                        } else if (dp[i + 1][j - 1]) { //情况三
+                            result++;
+                            dp[i][j] = true;
+                        }
+                    }
+                }
+            }
+            return result;*/
+        }
+    }
 //leetcode submit region end(Prohibit modification and deletion)
 
-  }
+}
